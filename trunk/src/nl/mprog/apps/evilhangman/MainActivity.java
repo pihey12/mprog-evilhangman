76f8245e1280c272212c02ff1bb19be97941967b
itsyzrz@gmail.com <itsyzrz@gmail.com@55668309-c4f3-50bb-45f6-e0b52834a79b>
2013-04-02 17:21:46 +0000

    Button aanmaken wat meer DRY

--- trunk/src/nl/mprog/apps/evilhangman/MainActivity.java
+++ trunk/src/nl/mprog/apps/evilhangman/MainActivity.java
@@ -3,11 +3,14 @@ package nl.mprog.apps.evilhangman;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	private static final int BUTTON_WIDTH = 59;
 	private static final int BUTTON_HEIGHT = 59;
@@ -19,38 +22,22 @@ public class MainActivity extends Activity {
 		
 		LinearLayout buttonLayout_1 = (LinearLayout) findViewById(R.id.button_layout_1);
 		for (char c = 'a'; c <= 'g'; c++) {
-			String text = c +"";
-			Button button = new Button(this);
-			button.setLayoutParams(new LayoutParams(BUTTON_WIDTH, BUTTON_HEIGHT));
-			button.setText(text.toUpperCase());
-			buttonLayout_1.addView(button);
+			buttonLayout_1.addView(createButton(c));
 		}
 		
 		LinearLayout buttonLayout_2 = (LinearLayout) findViewById(R.id.button_layout_2);
 		for (char c = 'h'; c <= 'n'; c++) {
-			String text = c +"";
-			Button button = new Button(this);
-			button.setLayoutParams(new LayoutParams(BUTTON_WIDTH, BUTTON_HEIGHT));
-			button.setText(text.toUpperCase());
-			buttonLayout_2.addView(button);
+			buttonLayout_2.addView(createButton(c));
 		}
 		
 		LinearLayout buttonLayout_3 = (LinearLayout) findViewById(R.id.button_layout_3);
 		for (char c = 'o'; c <= 'u'; c++) {
-			String text = c +"";
-			Button button = new Button(this);
-			button.setLayoutParams(new LayoutParams(BUTTON_WIDTH, BUTTON_HEIGHT));
-			button.setText(text.toUpperCase());
-			buttonLayout_3.addView(button);
+			buttonLayout_3.addView(createButton(c));
 		}
 		
 		LinearLayout buttonLayout_4 = (LinearLayout) findViewById(R.id.button_layout_4);
 		for (char c = 'v'; c <= 'z'; c++) {
-			String text = c +"";
-			Button button = new Button(this);
-			button.setLayoutParams(new LayoutParams(BUTTON_WIDTH, BUTTON_HEIGHT));
-			button.setText(text.toUpperCase());
-			buttonLayout_4.addView(button);
+			buttonLayout_4.addView(createButton(c));
 		}
 	}
 
@@ -59,6 +46,24 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	private Button createButton(char c) {
+		String text = c +"";
+		Button button = new Button(this);
+		button.setLayoutParams(new LayoutParams(BUTTON_WIDTH, BUTTON_HEIGHT));
+		button.setText(text.toUpperCase());
+		button.setOnClickListener(this);
+		
+		return button;
+	}
+
+	@Override
+	public void onClick(View v) {
+		Button button = (Button) v;
+		button.getText();
+		TextView textView = (TextView) findViewById(R.id.current_pogingen);
+		textView.setText("BUTTON "+ button.getText() +" CLICKED YO!");
 	}
 
 }
