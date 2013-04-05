316ebc5fc769f7bee1fc61f8b092331a9d5c283f
itsyzrz@gmail.com <itsyzrz@gmail.com@55668309-c4f3-50bb-45f6-e0b52834a79b>
2013-04-05 17:58:51 +0000

    Normale Hangman

---
+++ src/nl/mprog/apps/evilhangman/hangman/NormalHangman.java
@@ -0,0 +1,71 @@
+package nl.mprog.apps.evilhangman.hangman;
+
+public class NormalHangman implements Hangman {
+
+	@Override
+	public void addLetter(char letter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getWord() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCurrentWord() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getWordLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setWordLength(int length) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getGuesses() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setMaxGuesses(int maxGuesses) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean gameOver() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean gameWon() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void restart() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
