67812d1fdf931d450cc5b5662dbc6ebbfbb3cdd4
sebastiaan.jong@gmail.com <sebastiaan.jong@gmail.com@55668309-c4f3-50bb-45f6-e0b52834a79b>
2013-04-22 10:09:15 +0000

    test

---
+++ src/nl/mprog/apps/evilhangman/tests/HangmanTest.java
@@ -0,0 +1,69 @@
+package nl.mprog.apps.evilhangman.tests;
+
+import java.util.Arrays;
+import java.util.List;
+
+import junit.framework.TestCase;
+import nl.mprog.apps.evilhangman.hangman.EvilHangman;
+import nl.mprog.apps.evilhangman.hangman.Hangman;
+import nl.mprog.apps.evilhangman.hangman.NormalHangman;
+
+public class HangmanTest extends TestCase {
+	
+	public void testEvilHangman() {
+		List<String> words = Arrays.asList(new String[] {"abc", "def"});
+		Hangman hangman = new EvilHangman();
+		hangman.initializeWith(3, 2, words);
+		
+		hangman.addLetter('a');
+		assertFalse(hangman.gameOver());
+		assertFalse(hangman.gameWon());
+		assertEquals(hangman.getGuessesUsed(), 1);
+		
+		hangman.addLetter('d');
+		assertFalse(hangman.gameOver());
+		assertFalse(hangman.gameWon());
+		assertEquals(hangman.getGuessesUsed(), 1);
+		
+		hangman.addLetter('e');
+		assertFalse(hangman.gameOver());
+		assertFalse(hangman.gameWon());
+		assertEquals(hangman.getGuessesUsed(), 1);
+		
+		hangman.addLetter('b');
+		assertFalse(hangman.gameWon());
+		assertTrue(hangman.gameOver());
+		assertEquals(hangman.getGuessesUsed(), 2);
+	}
+	
+	public void testNormalHangman() {
+		List<String> words = Arrays.asList(new String[] {"abc"});
+		Hangman hangman = new NormalHangman();
+		hangman.initializeWith(3, 5, words);
+		
+		hangman.addLetter('a');
+		assertFalse(hangman.gameOver());
+		assertFalse(hangman.gameWon());
+		assertEquals(hangman.getGuessesUsed(), 0);
+		
+		hangman.addLetter('d');
+		assertFalse(hangman.gameOver());
+		assertFalse(hangman.gameWon());
+		assertEquals(hangman.getGuessesUsed(), 1);
+		
+		hangman.addLetter('b');
+		assertFalse(hangman.gameOver());
+		assertFalse(hangman.gameWon());
+		assertEquals(hangman.getGuessesUsed(), 1);
+		
+		hangman.addLetter('c');
+		assertFalse(hangman.gameOver());
+		assertTrue(hangman.gameWon());
+		assertEquals(hangman.getGuessesUsed(), 1);
+	}
+	
+	public void testEvilHangmanWithWordsFromDatabase() {
+		
+	}
+
+}
